9b4e2f4c34fbb11242eea3740a2cb4c5780d0d7b
Flavio Taho <flaviotaho@gmail.com>
2016-10-24 19:03:26 -0200

    :white_check_mark: Altera montagem de mock com iterates

--- test/mock/users-self-media-recent.mock.js
+++ test/mock/users-self-media-recent.mock.js
@@ -20,16 +20,17 @@ module.exports = function usersSelfMediaRecentMock() {
       })
       .reply(200, {
         pagination: {
-          next_url: 'https://api.instagram.com/v1/users/30990380/media/recent?access_token=30990380.31e9c33.aa42b1fd6f734d4ebf626f8eb5e315d7&count=33&max_id=1193500324263228984_30990380',
+          next_url: `https://api.instagram.com/v1/users/30990380/media/recent?access_token=${TEST_INSTAGRAM_ACCESS_TOKEN}&count=33&max_id=1193500324263228984_30990380`,
           next_max_id: '1193500324263228984_30990380',
         },
         meta: {
           code: 200,
         },
-        data: [
-          {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-          {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-        ],
+        data: Array.from(Array(33).keys()).map((item) => {
+          const initialDate = new Date('2016-11-23');
+          const datetime = (Date.parse(initialDate) - (item * 86400000)) / 1000;
+          return { created_time: datetime.toString() };
+        }),
       }, {
         'x-ratelimit-limit': '5000',
         'x-ratelimit-remaining': '4990',
@@ -45,7 +46,11 @@ module.exports = function usersSelfMediaRecentMock() {
         meta: {
           code: 200,
         },
-        data: [{}, {}, {}, {}, {}, {}, {}],
+        data: [Array.from(Array(7).keys()).map((item) => {
+          const initialDate = new Date('2016-10-28');
+          const datetime = (Date.parse(initialDate) - (item * 86400000)) / 1000;
+          return { created_time: datetime.toString() };
+        })],
       }, {
         'x-ratelimit-limit': '5000',
         'x-ratelimit-remaining': '4989',
