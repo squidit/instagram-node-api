4127c1c9e97519ec387cbf3dcefa061f3a244e85
Lucas Santos <lhs.santoss@gmail.com>
2018-01-11 16:52:37 -0200

    :sparkles: Add retry policy

--- src/instagram-functions/request-instagram.js
+++ src/instagram-functions/request-instagram.js
@@ -1,9 +1,13 @@
 const got = require('got')
 const parseResponse = require('../parse-response')
 
-function requestInstagram (url, options) {
+function requestInstagram (url, options, callCount = 0) {
   return got(url, options)
     .then(parseResponse)
+    .catch((err) => {
+      if (callCount <= require('../constants').MAX_RETRY_COUNT) requestInstagram(url, options, callCount + 1)
+      throw err
+    })
 }
 
 module.exports = requestInstagram
