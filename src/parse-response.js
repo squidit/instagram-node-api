06e519b2c8d98e71f3471c49dfc6d5808e1e5fe4
Lucas Santos <lhs.santoss@gmail.com>
2018-01-11 15:13:12 -0200

    :recycle: Remove lodash get
    No need to use it

--- src/parse-response.js
+++ src/parse-response.js
@@ -1,12 +1,10 @@
-const get = require('lodash/get')
-
 module.exports = function parseResponse (response) {
   try {
-    const data = get(response, 'body.data', null)
-    const pagination = get(response, 'body.pagination', null)
-    const meta = get(response, 'body.meta', null)
-    const remaining = parseInt(get(response, 'headers.x-ratelimit-remaining', 0), 10)
-    const limit = parseInt(get(response, 'headers.x-ratelimit-limit', 0), 10)
+    const data = response.body['data'] || null
+    const pagination = response.body['pagination'] || null
+    const meta = response.body['meta'] || null
+    const remaining = parseInt(response.headers['x-ratelimit-remaining'] || 0, 10)
+    const limit = parseInt(response.headers['x-ratelimit-limit'] || 0, 10)
 
     return Promise.resolve([data, pagination, meta, remaining, limit])
   } catch (error) {
