bc1a5cc968f13ecfdae03478ec8d5f97ff0cf6fe
Lucas Santos <lhs.santoss@gmail.com>
2018-01-12 16:09:07 -0200

    Adiciona política de tentativas (#21)
    * :wrench: Add gitignore
    
    * :sparkles: Add retry policy

--- src/instagram-functions/delete-to-instagram.js
+++ src/instagram-functions/delete-to-instagram.js
@@ -1,9 +1,13 @@
 const parseResponse = require('../parse-response')
 const got = require('got')
 
-function deleteToInstagram (url, options) {
+function deleteToInstagram (url, options, callCount = 0) {
   return got.delete(url, options)
     .then(parseResponse)
+    .catch((err) => {
+      if (callCount <= require('../constants').MAX_RETRY_COUNT) deleteToInstagram(url, options, callCount + 1)
+      throw err
+    })
 }
 
 module.exports = deleteToInstagram
--- src/instagram-functions/post-to-instagram.js
+++ src/instagram-functions/post-to-instagram.js
@@ -10,10 +10,14 @@ function parseBodyToJsonObject (response) {
   }
 }
 
-function postToInstagram (url, options) {
+function postToInstagram (url, options, callCount = 0) {
   return got.post(url, options)
     .then(parseBodyToJsonObject)
     .then(parseResponse)
+    .catch((err) => {
+      if (callCount <= require('../constants').MAX_RETRY_COUNT) postToInstagram(url, options, callCount + 1)
+      throw err
+    })
 }
 
 module.exports = postToInstagram
